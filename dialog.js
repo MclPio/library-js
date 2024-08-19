4398ef2155b572ae4eaa83321ba6a28d994ee3d6
MclPio <113801014+MclPio@users.noreply.github.com>
2024-08-18 20:07:02 -0400

    Add required form validation

--- dialog.js
+++ dialog.js
@@ -1,9 +1,10 @@
 // dialog
 const showButton = document.getElementById("showDialog");
-showButton.className = "button"
+showButton.className = "button";
 const favDialog = document.getElementById("favDialog");
 const outputBox = document.querySelector("output");
 const confirmBtn = favDialog.querySelector("#confirmBtn");
+const cancelBtn = favDialog.querySelector("#cancelBtn");
 
 // form inputs
 const inputTitle = document.getElementById("title");
@@ -16,10 +17,38 @@ showButton.addEventListener("click", () => {
   favDialog.showModal();
 });
 
-confirmBtn.addEventListener("click", (event) => {
+cancelBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  addedBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.checked);
-  addBookToLibrary(addedBook);
-  refreshTable();
   favDialog.close();
 });
+
+confirmBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (formValid()) {
+    addedBook = new Book(
+      inputTitle.value,
+      inputAuthor.value,
+      inputPages.value,
+      inputRead.checked
+    );
+    addBookToLibrary(addedBook);
+    refreshTable();
+    favDialog.close();
+  }
+});
+
+function formValid() {
+  if (inputTitle.validity.valueMissing) {
+    inputTitle.setCustomValidity("I am expecting a title");
+    inputTitle.reportValidity();
+  } else if (inputAuthor.validity.valueMissing) {
+    inputAuthor.setCustomValidity("I am expecting an author");
+    inputAuthor.reportValidity();
+  } else if (inputPages.validity.valueMissing) {
+    inputPages.setCustomValidity("I am expecting page number");
+    inputPages.reportValidity();
+  } else {
+    return true;
+  }
+  return false;
+}
